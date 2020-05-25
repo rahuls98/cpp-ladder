437721bdb8fb21838c9b0f7f0cc1794c84211502
rahuls98 <rahs98@gmail.com>
2020-05-25 19:12:21 +0530

    replace operation

--- Strings/operations.cpp
+++ Strings/operations.cpp
@@ -9,35 +9,41 @@ int main() {
     br();br();
 
     std::string s = "This is my message";
-    log("String: ", s);
-    log("String size: ", s.size());
+    log("String: ", s); //String: This is my message
+    log("String size: ", s.size()); //String size: 18
     br();
 
     //** clear **
     s.clear();
-    log("String size after clear: ", s.size());
+    log("String size after clear: ", s.size()); //String size after clear: 0
     br();
 
     s = "This is my message";
     //** erase **
     s.erase(8, 3);
-    log("String after erase 1: ", s);
+    log("String after erase 1: ", s); //String after erase 1: This is message
     s.erase( std::find(s.begin(), s.end(), ' ') );
-    log("String after erase 2: ", s);
+    log("String after erase 2: ", s); //String after erase 2: Thisis message
     s.erase(s.find(' '));
-    log("String after erase 3: ", s);
+    log("String after erase 3: ", s); //String after erase 3: Thisis
     br();
 
-    log("String size: ", s.size());
+    log("String size: ", s.size()); //String size: 6
     s.pop_back();
-    log("String after pop-back: ", s);
+    log("String after pop-back: ", s); //String after pop-back: Thisi
     s.push_back((int)toupper('s'));
-    log("String after push-back: ", s);
+    log("String after push-back: ", s); //String after push-back: ThisiS
     br();
 
     s += " my";
     s += {' ','m','e','s','s','a','g','e'};
-    log("String: ", s);
+    log("String: ", s); //String: ThisiS my message
+    br();
+
+    s.replace(5, 1, "s");
+    log("Replace using position: ", s); //Replace using position: Thisis my message
+    s.replace(s.begin(), s.begin()+6, "This is");
+    log("Replace using iterator: ", s); //Replace using iterator: This is my message
     br();
 
     return 0;
