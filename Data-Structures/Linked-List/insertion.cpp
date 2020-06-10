3ba45bf5ca5b2d66ca8d5c169cbe6fdf5e08127f
rahuls98 <rahs98@gmail.com>
2020-06-10 22:17:43 +0530

    insertion

---
+++ Data-Structures/Linked-List/insertion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+    Node(int data, Node* next) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+void frontInsert(int data) {
+    
+}
+void afterInsert() {
+    
+}
+void enInsert() {
+    
+}
+
+int main() {
+    Node* head = new Node()
+
+    return 0;
+}
