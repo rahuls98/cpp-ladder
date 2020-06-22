20672c5a5aec7ba48f9562b569a0ecfcc9c34578
rahuls98 <rahs98@gmail.com>
2020-06-22 15:31:31 +0530

    012 List Sort

--- Data-Structures/Linked-List/sort_012_list.cpp
+++ Data-Structures/Linked-List/sort_012_list.cpp
@@ -26,6 +26,18 @@ void insertNode(Node** head_ptr, int new_data) {
 void sortList(Node** head_ptr) {
     Node* temp = (*head_ptr);
     while(temp != NULL) {
+        if(mapper[temp->data] == 0) mapper[temp->data]=1;
+        else mapper[temp->data] += 1;
+
+        temp = temp->next;
+    }
+
+    temp = (*head_ptr);
+    for(auto p: mapper) {
+        for(int i=0; i<p.second; i++) {
+            temp->data = p.first;
+            temp = temp->next;
+        }
     }
 }
 
@@ -57,9 +69,10 @@ int main() {
     insertNode(&head, 0);
     insertNode(&head, 1);
 
-    print(&head);
+    print(&head);       // 1 1 2 0 2 0 1
     sortList(&head);
+    print(&head);       // 0 0 1 1 1 2 2
 
     delList(&head);
-    print(&head);
+    print(&head);       // Segmentation fault: 11
 }
