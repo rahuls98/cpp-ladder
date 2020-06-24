678bd9910a24912d3c5c2d84ddf055bb9659ce50
rahuls98 <rahs98@gmail.com>
2020-06-24 20:48:18 +0530

    Update MAP intro

--- STL-Containers/Map/intro.cpp
+++ STL-Containers/Map/intro.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <map>
 
+#define print(container) {for(auto x: container) {std::cout<<x.first<<": "<<x.second<<std::endl;}}
+#define br() {std::cout<<std::endl;}
+
 int main() {
     std::map<int, int> mapper = { {1,1} };
     mapper[0] = 2;
     mapper[2] = 1;
 
-    for(auto p: mapper) {
-        std::cout<<p.first<<": "<<p.second<<std::endl;
-    }
+    print(mapper); br();
+
+    int& ref = mapper.at(0);
+    ref = 5;
+
+    print(mapper); br();
 
     return 0;
 }
