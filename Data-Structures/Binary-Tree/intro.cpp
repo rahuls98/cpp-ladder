74f4b9e04e3c7f9206cfcb674692b6a3997962ac
rahuls98 <rahs98@gmail.com>
2020-06-26 22:01:26 +0530

    Binary Tree

---
+++ Data-Structures/Binary-Tree/intro.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* left = NULL;
+    Node* right = NULL;
+
+    Node(int data) {
+        this->data = data;
+    }
+};
+
+void preOrder(Node* ptr) {
+    if(ptr) {
+        std::cout<<ptr->data<<" ";
+        preOrder(ptr->left);
+        preOrder(ptr->right);
+    }
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    preOrder(root);
+
+    delete root->left->right;
+    delete root->left->left;
+    delete root->left;
+    delete root->right;
+    delete root;
+}
