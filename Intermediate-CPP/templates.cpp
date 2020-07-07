c1e2ab4726817a5ebf8da3890ec1a59c5e823aef
rahuls98 <rahs98@gmail.com>
2020-07-07 23:08:21 +0530

    templates

---
+++ Intermediate-CPP/templates.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdarg>
+
+template<typename T, int N>
+class Array {
+private:
+    T arr[N];
+public:
+    int size() { return N; }
+    void print() {
+        for(int i=0; i<N; i++) std::cout<<arr[i]<<" ";
+        std::cout<<std::endl;
+    }
+};
+
+int main() {
+    Array<char, 5> arr; 
+    std::cout<<arr.size()<<std::endl;
+
+    return 0;
+}
