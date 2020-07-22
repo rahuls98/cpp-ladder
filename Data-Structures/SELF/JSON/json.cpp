a0d001744cc4e449047b8503e0d69b6ff8f4f84a
rahuls98 <rahs98@gmail.com>
2020-07-22 20:16:32 +0530

    JSON with support for int,double,char,string valyes

--- Data-Structures/SELF/JSON/json.cpp
+++ Data-Structures/SELF/JSON/json.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <utility>
 #include <any>
 
+#define br() {std::cout<<std::endl;}
+
 class JSON {
-    std::vector<std::pair<std::any, std::any>> JSON_Obj;
+    std::vector< std::pair<std::any, std::any> > JSON_Obj;
+    std::unordered_map<std::string, int> hashString = {
+        {"NSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEE", 0},
+        {"c", 1},
+        {"i", 2},
+        {"d", 3}
+    };
+
+    void printCasted(std::any val) {
+        std::string key = std::string(val.type().name());
+        int c = hashString[key];
+        switch( c ) {
+            case 0 : std::cout<<std::any_cast<std::string>(val); break;
+            case 1 : std::cout<<std::any_cast<char>(val); break;
+            case 2 : std::cout<<std::any_cast<int>(val); break;
+            case 3 : std::cout<<std::any_cast<double>(val); break;
+            default: std::cout<<"Unknown type"; break;
+        }
+        return;
+    }
 
 public:
-    JSON() {
-        this->JSON_Obj = {};
-    }
+    JSON() { this->JSON_Obj = {}; }
 
     template<typename kT, typename vT>
     void insert(kT key, vT value) {
@@ -19,16 +39,46 @@ public:
 
     void print() {
         for(auto p: JSON_Obj) {
-            std::cout<<p.first.type().name()<<": "<<p.second.type().name()<<std::endl;
+            printCasted(p.first);
+            std::cout<<": ";
+            printCasted(p.second);
+            std::cout<<std::endl;
         }
+    }
+
+    void operator[](std::string ind) {
+        for(auto p: JSON_Obj) {
+            std::string key = std::any_cast<std::string>(p.first);
+            if(key == ind) {
+                printCasted(p.second);
+                return;
+            }
+        } 
     }
 };
 
 int main() {
-    JSON json;
-    json.insert("age",20);
-    json.insert("name","Rahul");
-    json.print();
+    JSON usn_188;
 
-    return 0;
+    usn_188.insert(std::string("Name"), std::string("Rahul"));
+    usn_188.insert(std::string("Age"), 21);
+    usn_188.insert(std::string("Gender"), 'M');
+    usn_188.insert(std::string("CGPA"), 8.19);
+    br();
+
+    usn_188.print();
+    /*
+        Name: Rahul
+        Age: 21
+        Gender: M
+        CGPA: 8.19
+    */
+    br();
+
+    usn_188["Name"]; br();      // Rahul
+    usn_188["Age"]; br();       // 21
+    usn_188["Gender"]; br();    // M
+    usn_188["CGPA"]; br();      // 8.19
+
+    br(); return 0;
 }
