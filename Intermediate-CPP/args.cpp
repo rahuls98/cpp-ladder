afd7e917852d9dfa0db91a4fae0da4c481fa8471
rahuls98 <rahs98@gmail.com>
2020-07-26 11:50:30 +0530

    Args.cpp update

--- Intermediate-CPP/args.cpp
+++ Intermediate-CPP/args.cpp
@@ -6,7 +6,7 @@ void mean(std::vector<T> args) {
     std::cout<<"Mean of args: ";
     int n = args.size();
 
-    if(n==0) std::cout<<0<<std::endl;
+    if(n==0) std::cout<<"UNDEFINED"<<std::endl;
     else if(n==1) std::cout<<args[0]<<std::endl;
     else {
         double total = 0;
@@ -16,10 +16,10 @@ void mean(std::vector<T> args) {
 }
 
 int main() {
-    mean<double>({});
-    mean<int>({ 10 });
-    mean<int>({ 1, 2, 3, 4, 5 });
-    mean<double>({ 1.54, 2.53, 6.12 });
+    mean<double>({});                       // Mean of args: UNDEFINED
+    mean<int>({ 10 });                      // Mean of args: 10
+    mean<int>({ 1, 2, 3, 4, 5 });           // Mean of args: 3 
+    mean<double>({ 1.54, 2.53, 6.12 });     // Mean of args: 3.39667
 
     return 0;
 }
